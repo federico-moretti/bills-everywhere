00d9af15d002e14fec4f56ee5580d8ee3832d371
Federico Moretti <hello@federicomoretti.dev>
2021-07-02 18:58:42 +0200

    feat: add merchants patch and categories get apis

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,9 +1,30 @@
-import { Merchant } from '../types';
+import { Category, Merchant } from '../types';
 
-const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'http://localhost:5000';
 
 export async function merchantsGet(): Promise<Merchant[] | undefined> {
   const res = await fetch(`${baseUrl}/merchants`);
   const payload: Merchant[] = await res.json();
+  return payload;
+}
+
+export async function merchantsPatch(
+  id: string,
+  merchant: Partial<Merchant>
+): Promise<Merchant | undefined> {
+  const res = await fetch(`${baseUrl}/merchants/${id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'PATCH',
+    body: JSON.stringify(merchant),
+  });
+  const payload: Merchant = await res.json();
+  return payload;
+}
+
+export async function categoriesGet(): Promise<Category[] | undefined> {
+  const res = await fetch(`${baseUrl}/categories`);
+  const payload: Category[] = await res.json();
   return payload;
 }
